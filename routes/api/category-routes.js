26150e825bc3bb21854d655570a6424bd6ed4b3b
molly <molly.salzberger@gmail.com>
2023-12-04 01:48:22 -0600

    category routes. try/catch for easier debugging?

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -25,28 +25,56 @@ router.get('/:id', (req, res) => {
     const payload = Category.findByPk(
       req.params.id,
       {
-        include: {model : Product}
+        include: { model: Product }
       }
     );
     res.status(200).json(payload)
   } catch (err) {
+    res.status(500).json(err)
+  }
+});
+
+router.post('/', async (req, res) => {
+  // create a new category
+
+  try {
+    const payload = await Category.create(req.body);
+    res.status(200).json(payload);
+  } catch (err) {
+    res.status(500).json(err)
 
   }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
-  const payload = Category.create(req.body);
-  res.json(payload);
-});
-
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
-  Category.update()
+  // TODO try/catch format
+  try {
+    const payload = await Category.update(
+      req.body,
+      {
+        where: {
+          id: req.params.id
+        }
+      });
+    res.status(200).json(payload)
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const payload = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 module.exports = router;
