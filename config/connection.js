f46a8f88fdffe12bd423c7b3e30f8aa38fe4de10
molly <molly.salzberger@gmail.com>
2023-12-04 01:54:13 -0600

    ...something in config needs to be fixed to work locally

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,26 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+// const sequelize = process.env.JAWSDB_URL
+//   ? new Sequelize(process.env.JAWSDB_URL)
+//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//       host: 'localhost',
+//       dialect: 'mysql',
+//       dialectOptions: {
+//         decimalNumbers: true,
+//       },
+//     });
+// the above is for deploying to heroku
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {//where to connect
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+  }
+);
 
 module.exports = sequelize;
