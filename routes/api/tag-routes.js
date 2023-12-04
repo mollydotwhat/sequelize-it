6ebad54b075afc1b4ed62ff348bef0498ca3a9d1
molly <molly.salzberger@gmail.com>
2023-12-04 01:49:43 -0600

    putting in try/catch format FIRST

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,23 +6,48 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+
+  } catch (err) {
+
+  }
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+
+  } catch (err) {
+    
+  }
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  try {
+
+  } catch (err) {
+    
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  try {
+
+  } catch (err) {
+    
+  }
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  try {
+
+  } catch (err) {
+    
+  }
 });
 
 module.exports = router;
